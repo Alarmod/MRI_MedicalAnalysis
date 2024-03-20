bdce16f8060318cac8f8fc20a6dbb22213b66ea7
Alarmod <alarmod85@hotmail.com>
2024-03-20 10:28:06 +0300

    Move libs to https://github.com/Alarmod/dicomlib and https://github.com/Alarmod/flann

--- ExtractYoloObjectsFromDICOM/flann/build/src/cpp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/My programs/ExtractYoloObjectsFromDICOM/flann/src/cpp
-# Build directory: F:/My programs/ExtractYoloObjectsFromDICOM/flann/build/src/cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
