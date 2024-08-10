a06ad227c6bf8c7fd135cfdaddc9fde0e19b29d2
Alarmod <alarmod85@hotmail.com>
2024-08-10 07:17:15 +0300

    Added bat-files for clocks control

---
+++ clocks_control/set_clocs_1650.bat
@@ -0,0 +1,15 @@
+@echo off
+
+cd /d %~dp0
+
+@RUN rigel.exe must be run with administrator rights
+
+start /B rigel.exe -a kawpow --cclock 15 --lock-cclock 1980 --mclock 250 --lock-mclock 22000 --pl 85 -o [1]stratum+tcp://dummy.server.com:3112 -u X --no-tui --no-watchdog
+
+TIMEOUT /T 7
+
+wmic process where name="rigel.exe" terminate >nul 2> nul
+
+cls
+
+exit
