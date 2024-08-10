79617c56200db51efc359f65521a7085ed201189
Alarmod <alarmod85@hotmail.com>
2024-08-10 07:22:27 +0300

    Fix tupo

--- clocks_control/set_clocs_1650.bat
+++ clocks_control/set_clocs_1650.bat
@@ -2,7 +2,7 @@
 
 cd /d %~dp0
 
-@RUN rigel.exe must be run with administrator rights
+@REM rigel.exe must be run with administrator rights
 
 start /B rigel.exe -a kawpow --cclock 15 --lock-cclock 1980 --mclock 250 --lock-mclock 22000 --pl 85 -o [1]stratum+tcp://dummy.server.com:3112 -u X --no-tui --no-watchdog
 
--- clocks_control/set_clocs_3070.bat
+++ clocks_control/set_clocs_3070.bat
@@ -2,7 +2,7 @@
 
 cd /d %~dp0
 
-@RUN rigel.exe must be run with administrator rights
+@REM rigel.exe must be run with administrator rights
 
 start /B rigel.exe -a kawpow --cclock 15 --lock-cclock 1920 --mclock 1000 --lock-mclock 22000 --pl 220 -o [1]stratum+tcp://dummy.server.com:3112 -u X --no-tui --no-watchdog
 
