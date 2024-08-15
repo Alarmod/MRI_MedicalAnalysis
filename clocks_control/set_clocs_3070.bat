193b26352243188908222552e20b991cc3d04ee5
Vitaly Fralenko <alarmod@pereslavl.ru>
2024-08-15 07:54:32 +0300

    Update set_clocs_3070.bat

--- clocks_control/set_clocs_3070.bat
+++ clocks_control/set_clocs_3070.bat
@@ -4,7 +4,7 @@ cd /d %~dp0
 
 @REM rigel.exe must be run with administrator rights
 
-start /B rigel.exe -a kawpow --cclock 15 --lock-cclock 1920 --mclock 1000 --lock-mclock 22000 --pl 220 -o [1]stratum+tcp://dummy.server.com:3112 -u X --no-tui --no-watchdog
+start /B rigel.exe -a kawpow --cclock 15 --lock-cclock 1920 --mclock 750 --lock-mclock 22000 --pl 220 -o [1]stratum+tcp://dummy.server.com:3112 -u X --no-tui --no-watchdog
 
 TIMEOUT /T 15
 
