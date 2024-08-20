5d18e3b43903457be189ff67864bb3e5c288c050
Alarmod <alarmod85@hotmail.com>
2024-08-20 08:44:16 +0300

    Fix DLL loader

--- automated_workplace/cracks_segmentator/02_test.py
+++ automated_workplace/cracks_segmentator/02_test.py
@@ -8,11 +8,14 @@ libs_path=["D:/libs/opencv-4_9_0/build/x64/vc16/bin",
 		"D:/libs/cuda/11.8/bin", 
 		"D:/libs/cudnn/8.9.7.29/bin"]
 
+path_fix = []
 for path in libs_path:
-	if os.path.exists(path):
+	if os.path.isdir(path):
+		path_fix.append(path)
 		os.add_dll_directory(path)
 	else:
 		print(f"Warning: Path \'{path}\' not exists")
+os.environ["PATH"] = os.pathsep.join(path_fix) + os.pathsep + os.environ["PATH"]
 
 import yolo_segment_ext as yolo_inference
 yolo_inference.setGlobalThreadPoolSize(4)
